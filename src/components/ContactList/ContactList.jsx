662d73f3cd97c908d2dabf0cabdd17fca39d3cdd
Musteks555 <artemmustek73@gmail.com>
2024-03-11 12:15:22 +0200

    add task

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,15 @@
-const ContactList = () => {
-    return <div>ContactList</div>;
+import Contact from "../Contact/Contact";
+
+import css from "./ContactList.module.css";
+
+const ContactList = ({ contactsData, deleteContact }) => {
+    return (
+        <div className={css.conctactList}>
+            {contactsData.map(({ id, name, number }) => {
+                return <Contact key={id} contactName={name} contactNumber={number} contactId={id} deleteContact={deleteContact} />;
+            })}
+        </div>
+    );
 };
 
 export default ContactList;
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,14 @@
-const SearchBox = () => {
-    return <div>SearchBox</div>;
+import css from "./SearchBox.module.css";
+
+const SearchBox = ({ value, onFilter }) => {
+    return (
+        <>
+            <label className={css.contactsLabel}>
+                <span>Find contacts by name</span>
+                <input type="text" value={value} onChange={(event) => onFilter(event.target.value)} />
+            </label>
+        </>
+    );
 };
 
 export default SearchBox;
